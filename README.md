f66935dffc3bd7452e7fa5f3b5f8d83af05bb5b7
tani-rana005 <tanirana0510@gmail.com>
2025-12-12 22:44:23 +0530

    Add new photo

--- README.md
+++
@@ -1,2 +0,0 @@
-# Food_order_project
-I am building my first full stack project.
---
+++ script.js
@@ -0,0 +1,18 @@
+// let a = document.querySelector("div");
+// console.log(a.setAttribute("name","tanisha"));
+
+// let b = a.getAttribute("id");
+// console.log(b);
+
+// let c = a.getAttribute("name");
+// console.log(c);
+
+//a.style.backgroundColor = "white";
+//a.style.fontSize = "200px";
+
+// let btn = document.createElement("button");
+// btn.innerText = "tanisha";
+// console.log(btn);
+
+//  let a = document.querySelector("div");
+//  a.prepend(btn);
